fb42b53ea1b93b323319815bbe752138e687ac96
MarySoroka <44846587+MarySoroka@users.noreply.github.com>
2019-12-18 01:11:25 +0300

    load new map, add moving platforms, add killing enemy

--- main.cpp
+++ main.cpp
@@ -1,14 +1,11 @@
 #include <SFML/Graphics.hpp>
-//#include "map.h"
 #include "view.h"
 #include <iostream>
-#include <sstream>
 #include "mission.h"
 #include "iostream"
 #include "level.h"
 #include <vector>
 #include <list>
-
 #include "TinyXML/tinyxml.h"
 
 using namespace sf;
@@ -34,6 +31,7 @@ public:
 	FloatRect getRect() {//ф-ция получения прямоугольника. его коорд,размеры (шир,высот).
 		return FloatRect(x, y, w, h);//эта ф-ция нужна для проверки столкновений 
 	}
+	virtual void update(float time) = 0;
 };
 ////////////////////////////////////////////////////КЛАСС ИГРОКА////////////////////////
 class Player :public Entity {
@@ -107,6 +105,25 @@ public:
 		if (life) { setPlayerCoordinateForView(x, y); }
 		dy = dy + 0.0015 * time;
 	}
+
+	
+};
+
+
+class MovingPlatform : public Entity {//класс движущейся платформы
+public:
+	MovingPlatform(Image& image, String Name, Level& lvl, float X, float Y, int W, int H) :Entity(image, Name, X, Y, W, H) {
+		sprite.setTextureRect(IntRect(0, 0, W, H));//прямоугольник 
+		dx = 0.08;//изначальное ускорение по Х
+	}
+
+	void update(float time)//функция обновления платформы.
+	{
+		x += dx * time;//реализация движения по горизонтали
+		moveTimer += time;//наращиваем таймер
+		if (moveTimer > 2000) { dx *= -1; moveTimer = 0; }//если прошло примерно 2 сек, то меняется направление движения платформы,а таймер обнуляется
+		sprite.setPosition(x + w / 2, y + h / 2);//задаем позицию спрайту
+	}
 };
 
 
@@ -115,7 +132,7 @@ class Enemy :public Entity {
 public:
 	Enemy(Image& image, String Name, Level& lvl, float X, float Y, int W, int H) :Entity(image, Name, X, Y, W, H) {
 		obj = lvl.GetObjects("solid");//инициализируем.получаем нужные объекты для взаимодействия врага с картой
-		if (name == "EasyEnemy") {
+		if (name == "easyEnemy") {
 			sprite.setTextureRect(IntRect(0, 0, w, h));
 			dx = 0.1;
 		}
@@ -137,7 +154,7 @@ public:
 
 	void update(float time)
 	{
-		if (name == "EasyEnemy") {
+		if (name == "easyEnemy") {
 			//moveTimer += time;if (moveTimer>3000){ dx *= -1; moveTimer = 0; }//меняет направление примерно каждые 3 сек
 			checkCollisionWithMap(dx, 0);
 			x += dx * time;
@@ -149,8 +166,8 @@ public:
 
 int main()
 {
-	RenderWindow window(VideoMode(640, 480), "SELSOR");
-	view.reset(FloatRect(0, 0, 640, 480));
+	RenderWindow window(VideoMode(640, 380), "SELSOR");
+	view.reset(FloatRect(0, 0, 640, 380));
 
 	Level lvl;//создали экземпляр класса уровень
 	lvl.LoadFromFile("map.tmx");//загрузили в него карту, внутри класса с помощью методов он ее обработает.
@@ -162,12 +179,28 @@ int main()
 	easyEnemyImage.loadFromFile("images/man.png");
 	easyEnemyImage.createMaskFromColor(Color(255, 0, 0));
 
+	Image movePlatformImage;
+	movePlatformImage.loadFromFile("images/movingPlatform.png");
+
 	Object player = lvl.GetObject("player");//объект игрока на нашей карте.задаем координаты игроку в начале при помощи него
-	Object easyEnemyObject = lvl.GetObject("easyEnemy");//объект легкого врага на нашей карте.задаем координаты игроку в начале при помощи него
 
 	Player p(heroImage, "Player1", lvl, player.rect.left, player.rect.top, 40, 30);//передаем координаты прямоугольника player из карты в координаты нашего игрока
-	Enemy easyEnemy(easyEnemyImage, "EasyEnemy", lvl, easyEnemyObject.rect.left, easyEnemyObject.rect.top, 200, 97);//передаем координаты прямоугольника easyEnemy из карты в координаты нашего врага
 
+	std::list<Entity*>  entities;//создаю список, сюда буду кидать объекты.например врагов.
+	std::list<Entity*>::iterator it;//итератор чтобы проходить по эл-там списка
+	std::list<Entity*>::iterator it2;//второй итератор.для взаимодействия между объектами списка
+
+	std::vector<Object> e = lvl.GetObjects("easyEnemy");//все объекты врага на tmx карте хранятся в этом векторе
+
+	for (int i = 0; i < e.size(); i++){//проходимся по элементам этого вектора(а именно по врагам)
+		entities.push_back(new Enemy(easyEnemyImage, "easyEnemy", lvl, e[i].rect.left, e[i].rect.top, 40, 65));//и закидываем в список всех наших врагов с карты
+	    e[i].rect.left;//коорд Х
+		e[i].rect.top;//коорд Y
+	}
+
+	e = lvl.GetObjects("movingPlatform");//забираем все платформы в вектор 
+
+	for (int i = 0; i < e.size(); i++) entities.push_back(new MovingPlatform(movePlatformImage, "MovingPlatform", lvl, e[i].rect.left, e[i].rect.top, 95, 22));//закидываем платформу в список.передаем изображение имя уровень координаты появления (взяли из tmx карты), а так же размеры
 	Clock clock;
 	while (window.isOpen())
 	{
@@ -184,12 +217,56 @@ int main()
 				window.close();
 		}
 		p.update(time);
-		easyEnemy.update(time);
+		for (it = entities.begin(); it != entities.end();)//говорим что проходимся от начала до конца
+		{
+			Entity* b = *it;//для удобства, чтобы не писать (*it)->
+			b->update(time);//вызываем ф-цию update для всех объектов (по сути для тех, кто жив)
+			if (b->life == false) { it = entities.erase(it); delete b; }// если этот объект мертв, то удаляем его
+			else it++;//и идем курсором (итератором) к след объекту. так делаем со всеми объектами списка
+		}
+		for (it = entities.begin(); it != entities.end(); it++)//проходимся по эл-там списка
+		{
+			if ((*it)->getRect().intersects(p.getRect()))//если прямоугольник спрайта объекта пересекается с игроком
+			{
+				if ((*it)->name == "easyEnemy") {//и при этом имя объекта EasyEnemy,то..
+				////////выталкивание врага
+					if ((*it)->dx > 0)//если враг идет вправо
+					{
+						std::cout << "(*it)->x" << (*it)->x << "\n";//коорд игрока
+						std::cout << "p.x" << p.x << "\n\n";//коорд врага
+						(*it)->x = p.x - (*it)->w; //отталкиваем его от игрока влево (впритык)
+						(*it)->dx = 0;//останавливаем
+						std::cout << "new (*it)->x" << (*it)->x << "\n";//новая коорд врага
+						std::cout << "new p.x" << p.x << "\n\n";//новая коорд игрока (останется прежней)
+					}
+					if ((*it)->dx < 0)//если враг идет влево
+					{
+						(*it)->x = p.x + p.w; //аналогично - отталкиваем вправо
+						(*it)->dx = 0;//останавливаем
+					}
+					///////выталкивание игрока
+					if (p.dx < 0) { p.x = (*it)->x + (*it)->w; }//если столкнулись с врагом и игрок идет влево то выталкиваем игрока
+					if (p.dx > 0) { p.x = (*it)->x - p.w; }//если столкнулись с врагом и игрок идет вправо то выталкиваем игрока
+				}
+			}
+			for (it2 = entities.begin(); it2 != entities.end(); it2++) {
+				if ((*it)->getRect() != (*it2)->getRect())//при этом это должны быть разные прямоугольники
+					if (((*it)->getRect().intersects((*it2)->getRect())) && ((*it)->name == "EasyEnemy") && ((*it2)->name == "EasyEnemy"))//если столкнулись два объекта и они враги
+					{
+						(*it)->dx *= -1;//меняем направление движения врага
+						(*it)->sprite.scale(-1, 1);//отражаем спрайт по горизонтали
+					}
+			}
+		}
+		
 		window.setView(view);
 		window.clear(Color(77, 83, 140));
-		lvl.Draw(window);//рисуем новую карту
+		lvl.Draw(window);
 
-		window.draw(easyEnemy.sprite);
+		for (it = entities.begin(); it != entities.end(); it++) {
+			window.draw((*it)->sprite); //рисуем entities объекты (сейчас это только враги)
+		}
+		//window.draw(easyEnemy.sprite);//старый вариант рисования одного врага
 		window.draw(p.sprite);
 		window.display();
 	}
